b716623a93ba6234a80eaeebe2466e1d25a51d8e
Huseyin <huseyin.ar@kgteknoloji.com>
2019-09-30 15:12:04 +0300

    admin logout process

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../modelss/auth.service';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 
 export class AdminComponent {
 
+        constructor(private authService: AuthService, private router: Router) {}
+
+        logout() {
+             this.authService.clear();
+             this.router.navigateByUrl('/shop');
+        }
 }
